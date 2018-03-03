a3cb254ba4ae0276e78293d3e862443fdcea8a6a
Zeb Matteson <zmatteson@gmail.com>
2018-03-02 20:37:05 -0600

    small changes

--- chapter_8/8_1.py
+++ chapter_8/8_1.py
@@ -2,6 +2,7 @@
 Design a stack that includes a max operation, in addition to push and 
 pop.  The max method should return the maximum value in the stack.
 """
+
 #It's pretty trivial to implement a stack.  We can either track the int,
 #or the index of the element that is the max stack.
 
